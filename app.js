c29cc9b0265cdd9f58f29b0e225b5cdbd40d3805
Andreas Daskalopoulos <Andreas.Daskalopoulos@citrix.com>
2018-06-04 17:29:03 +0300

    Code refactoring

--- app.js
+++ app.js
@@ -1,50 +1,31 @@
-const github = new Github();
+// Init Github
+const github = new Github;
+// Init UI
+const ui = new UI;
 
-document.getElementById('searchUser').addEventListener('keyup', searchUser)
+// Search input
+const searchUser = document.getElementById('searchUser');
 
-function searchUser(e) {
-    console.log('Search');
-    const userText = e.target.value;
-    console.log(`Text to search: ${userText}`);
-    if (userText !== null) {
-        return github.getUser(userText).then(res => {
-            if (res.profile.message === 'Not Found') {
-                // Show alert
-                console.log(`User ${userText} not found`);
-            } else {
-                // Show profile
-                console.log(res.profile);
-                const profile = document.getElementById('profile');
-                const user = res.profile;
-                profile.innerHTML = `
-                <div class="card card-body mb-3">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img class="img-fluid mb-2" src="${user.avatar_url}">
-                            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
-                        </div>
-                        <div class="col-md-9">
-                            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-                            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-                            <span class="badge badge-success">Followers: ${user.followers}</span>
-                            <span class="badge badge-info">Following: ${user.following}</span>
-                            <br><br>
-                            <ul class="list-group">
-                            <li class="list-group-item">Company: ${user.company}</li>
-                            <li class="list-group-item">Website/Blog: ${user.blog}</li>
-                            <li class="list-group-item">Location: ${user.location}</li>
-                            <li class="list-group-item">Member Since: ${user.created_at}</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <h3 class="page-heading mb-3">Latest Repos</h3>
-                <div id="repos"></div>
-                `;   
-            }
-        })
-    } else {
-        // Clear profile
-    }
-    e.preventDefault();
-}
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+}); 
